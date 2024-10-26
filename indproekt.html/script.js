6e5fb0607ed9a3a23345cb49d4b9af7c72c1d019
nesquii29 <loxdati@gmail.com>
2024-10-26 11:15:23 +0300

    Delete indproekt.html directory

--- indproekt.html/script.js
+++
@@ -1,9 +0,0 @@
-// Простой интерактивный скрипт для отображения фона при наведении
-document.querySelectorAll('section').forEach(section => {
-    section.addEventListener('mouseenter', () => {
-        section.style.backgroundColor = "#f0f8ff";
-    });
-    section.addEventListener('mouseleave', () => {
-        section.style.backgroundColor = "#fff";
-    });
-});
